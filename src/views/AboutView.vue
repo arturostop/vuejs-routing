ac1f506339c6ff1e1d471f934b12c13bee31e109
arturostop <arturo_parada_95@hotmal.com>
2022-11-17 11:23:06 -0700

    web-jobs

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo voluptatum incidunt, enim tenetur non voluptates, iusto ducimus iste nesciunt error nihil architecto, labore repellat debitis. Expedita quisquam nesciunt inventore quod odit saepe adipisci corrupti illo, quae, earum amet fuga sint consequatur aliquid aut voluptates alias, nobis totam! Autem harum vel necessitatibus alias reiciendis perferendis optio ratione sed laboriosam temporibus delectus neque voluptatibus, numquam facilis in eveniet, sit corporis et ipsam aut rem consequuntur ut. Esse cupiditate aspernatur debitis repudiandae accusantium ea nulla magnam. Sequi quae excepturi cupiditate hic similique illum nemo et omnis, praesentium voluptas commodi dignissimos. Dignissimos, vitae aperiam.</p>
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,14 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In illo praesentium inventore. Nulla ab odit ut natus aspernatur asperiores laboriosam velit facere! Quam voluptatem consequatur animi provident excepturi at magni facere error iste earum eaque eos reiciendis fugit, qui, sequi, esse molestias quod. Culpa veritatis eos ipsa beatae optio id. Corporis, non quasi, quidem cupiditate laudantium ea dolore repellendus obcaecati soluta autem enim? Et, velit perspiciatis quam ea porro reprehenderit. Ipsam voluptatem quia dolore odio beatae numquam vel quidem doloribus magnam ad distinctio quis tempore, provident id odit harum libero consequuntur sed officia culpa totam. Enim exercitationem porro tenetur eum.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis eius officia esse voluptatem, tempora tenetur assumenda inventore asperiores? Quasi natus, veniam obcaecati suscipit at totam adipisci doloremque quo, deleniti doloribus corporis ipsam molestiae eligendi excepturi ex, distinctio similique ratione? Accusamus quis quas sit iure possimus, quia deserunt molestiae aspernatur autem quos omnis aliquam ipsa ipsum expedita nostrum maiores repudiandae voluptas et nulla hic minima, repellendus quam! Quam reprehenderit natus ipsa iste quia quos, cum, animi ad beatae porro, rerum mollitia omnis. Beatae placeat doloremque nostrum similique ducimus, eaque minus earum dolor voluptas. Explicabo iure quasi corporis perspiciatis beatae accusantium odit.</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  components: {  }
 }
 </script>
